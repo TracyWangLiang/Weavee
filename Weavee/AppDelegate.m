5c125559bcbf2c92d7aba8876ee8c40473c3df15
Wang <Wang12345@163.com>
2025-11-06 17:28:36 +0800

    移除通知

--- Weavee/AppDelegate.m
+++ Weavee/AppDelegate.m
@@ -17,67 +17,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
-    [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert | UNAuthorizationOptionSound | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error) {
-        if (granted) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [[UIApplication sharedApplication] registerForRemoteNotifications];
-            });
-        }
-    }];
-//    [[FBSDKApplicationDelegate sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
-//    [self fuseFlareVaultWithWeftSpire];
     
     return YES;
 }
-
-
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-    
-    const unsigned char *tokenBytes = deviceToken.bytes;
-    NSMutableString *hexToken = [NSMutableString stringWithCapacity:deviceToken.length * 2];
-    for (NSUInteger i = 0; i < deviceToken.length; i++) {
-        [hexToken appendFormat:@"%02x", tokenBytes[i]];
-    }
-    /// pushToken
-    Weavee * weavee = [[Weavee alloc] init];
-    [weavee twistAuricLatticeWithEchoShard:hexToken prismWeftPulse:@"lumenDriftCore"];
-    
-}
--(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
-    
-}
-
-//#pragma mark - ad
-//-(void)fuseFlareVaultWithWeftSpire {
-//    [Adjust requestAppTrackingAuthorizationWithCompletionHandler:^(NSUInteger status) {
-//        switch (status) {
-//            case ATTrackingManagerAuthorizationStatusNotDetermined:
-//                NSLog(@"Tracking: Not Determined");
-//                break;
-//            case ATTrackingManagerAuthorizationStatusRestricted:
-//                NSLog(@"Tracking: Restricted");
-//                break;
-//            case ATTrackingManagerAuthorizationStatusDenied:
-//                NSLog(@"Tracking: Denied");
-//                break;
-//            case ATTrackingManagerAuthorizationStatusAuthorized:
-//                NSLog(@"Tracking: Authorized");
-//                break;
-//        }
-//    }];
-//    
-//    NSString *environment = ADJEnvironmentSandbox;
-//    ADJConfig *adjustConfig = [[ADJConfig alloc] initWithAppToken:@"oypt3hjxs0e8" environment:environment];
-//    [adjustConfig setAttConsentWaitingInterval:60];
-//    [adjustConfig disableAppTrackingTransparencyUsage];
-//    [Adjust initSdk:adjustConfig];
-//    
-//    [Adjust attributionWithCompletionHandler:^(ADJAttribution * _Nullable attribution) {
-//        [Adjust trackEvent:[[ADJEvent alloc] initWithEventToken:@"rb2914"]];
-//    }];
-//    
-//}
 
 
 #pragma mark - UISceneSession lifecycle
